7467092d828b6d679d9c91843306d5f1a36174b0
DanielG <Danielmorrisg@gmail.com>
2018-08-10 07:23:11 -0700

    added db sql code... bob's burgers themedgit add -A

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ("Sgt. Poblano Pepper Lonely Artichoke Hearts Club Burger");
+INSERT INTO burgers (name) VALUES ("Chipotle Off the Old Block Burger");
+INSERT INTO burgers (name) VALUES ("Eggers Can't Be Cheesers ");
+INSERT INTO burgers (name) VALUES ("Thank God It's Fried Egg Burger");
+INSERT INTO burgers (name) VALUES ("Don't You Four Cheddar 'Bout Me Burger");
+
