4bf61d59f3d672a10e4e7a40f4dec57f74143db3
DanielG <Danielmorrisg@gmail.com>
2018-08-10 20:29:26 -0700

    updated connection.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ("Sgt. Poblano Pepper Lonely Artichoke Hearts Club Burger");
-INSERT INTO burgers (name) VALUES ("Chipotle Off the Old Block Burger");
-INSERT INTO burgers (name) VALUES ("Eggers Can't Be Cheesers ");
-INSERT INTO burgers (name) VALUES ("Thank God It's Fried Egg Burger");
-INSERT INTO burgers (name) VALUES ("Don't You Four Cheddar 'Bout Me Burger");
+INSERT INTO burgers (burger_name) VALUES ("Sgt. Poblano Pepper Lonely Artichoke Hearts Club Burger");
+INSERT INTO burgers (burger_name) VALUES ("Chipotle Off the Old Block Burger");
+INSERT INTO burgers (burger_name) VALUES ("Eggers Can't Be Cheesers ");
+INSERT INTO burgers (burger_name) VALUES ("Thank God It's Fried Egg Burger");
+INSERT INTO burgers (burger_name) VALUES ("Don't You Four Cheddar 'Bout Me Burger");
 
