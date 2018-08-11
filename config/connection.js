057a841d62dafe69ee7c30fdaa3981cf0642eebb
DanielG <Danielmorrisg@gmail.com>
2018-08-11 07:54:39 -0700

    fixed port issue for localhost

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 8080,
+  port: 3306,
   user: "root",
   password: "root",
   database: "burgers_db"
